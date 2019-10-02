35e5c607914bf98c92dc3a32a4360c4fbdb99074
Adriano Chambel <acmlima.softweb@gmail.com>
2019-10-02 19:04:49 +0100

    Input and Button Generics are done.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+import Background from '~/components/Background/';
+
 // import { Container } from './styles';
 
-export default function Signin() {
-  return <Text>Signin</Text>;
+export default function SignIn() {
+  return (
+    <Background>
+      <Text>Signin</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Fill your name"
+      />
+      <Button>Enter</Button>
+    </Background>
+  );
 }
