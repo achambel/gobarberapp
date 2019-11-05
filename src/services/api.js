082815bc62c92b7c8ac0d747ccd17b03fc2a4271
Adriano Chambel <acmlima.softweb@gmail.com>
2019-11-05 23:43:22 +0000

    App done

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
+import { Platform } from 'react-native';
+
+let url;
+
+if (Platform.OS === 'ios') {
+  url = 'http://localhost:3333';
+} else if (Platform.OS === 'android') {
+  url = 'http://10.0.2.2:3333';
+}
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: url,
 });
 
 export default api;
